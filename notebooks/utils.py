45b386182fb207263333c3bf683c3ff8cff70206
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-03-30 09:18:23 -0400

    Move demographics files, update utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -5,59 +5,6 @@ import pandas as pd
 import numpy as np
 from os.path import join
 
-<<<<<<< HEAD
-from constants import DATA_DIR, COLUMNS
-
-"""
-Utilities for reading in data from all sites for each file type.
-"""
-
-def read_full_df(prefix, site_ids):
-    return pd.concat((pd.read_csv(join(DATA_DIR, f"{prefix}-{site_id}.csv"), header=None) for site_id in site_ids), ignore_index=True)
-
-def read_full_daily_counts_df(site_ids):
-    return read_full_df("DailyCounts", site_ids).rename(columns={
-        0: COLUMNS.SITE_ID,
-        1: COLUMNS.DATE,
-        2: COLUMNS.NEW_POSITIVE_CASES,
-        3: COLUMNS.PATIENTS_IN_ICU,
-        4: COLUMNS.NEW_DEATHS
-    })
-
-def read_full_demographics_df(site_ids):
-    return read_full_df("Demographics", site_ids).rename(columns={
-        0: COLUMNS.SITE_ID,
-        1: COLUMNS.SEX,
-        2: COLUMNS.TOTAL_PATIENTS,
-        3: COLUMNS.AGE_0TO2,
-        4: COLUMNS.AGE_3TO5,
-        5: COLUMNS.AGE_6TO11,
-        6: COLUMNS.AGE_12TO17,
-        7: COLUMNS.AGE_18TO25,
-        8: COLUMNS.AGE_26TO49,
-        9: COLUMNS.AGE_50TO69,
-        10: COLUMNS.AGE_70TO79,
-        11: COLUMNS.AGE_80PLUS
-    })
-
-def read_full_diagnoses_df(site_ids):
-    return read_full_df("Diagnoses", site_ids).rename(columns={
-        0: COLUMNS.SITE_ID,
-        1: COLUMNS.ICD_CODE,
-        2: COLUMNS.ICD_VERSION,
-        3: COLUMNS.NUM_PATIENTS
-    })
-
-def read_full_labs_df(site_ids):
-    return read_full_df("Labs", site_ids).rename(columns={
-        0: COLUMNS.SITE_ID,
-        1: COLUMNS.LOINC,
-        2: COLUMNS.DAYS_SINCE_POSITIVE,
-        3: COLUMNS.NUM_PATIENTS,
-        4: COLUMNS.MEAN_VALUE,
-        5: COLUMNS.STDEV_VALUE
-    })
-=======
 from constants import (
     COLUMNS,
     DATA_DIR,
@@ -66,7 +13,6 @@ from constants import (
     SITE_FILE_TYPES, 
     ALL_SITE_FILE_TYPES
 )
->>>>>>> e7c595104cc951e9bc296e34a6bb1f91b80a79dd
 
 """
 Utilities for listing site file information.
@@ -140,13 +86,13 @@ def read_full_demographics_df(site_file_info):
         COLUMNS.TOTAL_PATIENTS,
         COLUMNS.AGE_0TO2,
         COLUMNS.AGE_3TO5,
-        AGE_6TO11,
-        AGE_12TO17,
-        AGE_18TO25,
-        AGE_26TO49,
-        AGE_50TO69,
-        AGE_70TO79,
-        AGE_80PLUS
+        COLUMNS.AGE_6TO11,
+        COLUMNS.AGE_12TO17,
+        COLUMNS.AGE_18TO25,
+        COLUMNS.AGE_26TO49,
+        COLUMNS.AGE_50TO69,
+        COLUMNS.AGE_70TO79,
+        COLUMNS.AGE_80PLUS
     ])
 
 def read_full_diagnoses_df(site_file_info):
