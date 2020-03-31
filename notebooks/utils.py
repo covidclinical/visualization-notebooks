990f57c47c57888a279e45990049533997e3c053
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-03-31 15:36:00 -0400

    Move util functions to utils.py

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,9 +1,10 @@
 import re
 import glob
-import datetime
 import pandas as pd
 import numpy as np
+import datetime
 from os.path import join
+
 
 from constants import (
     COLUMNS,
@@ -137,3 +138,75 @@ def read_icd_df():
 
 def read_loinc_df():
     return pd.read_csv(join(DATA_DIR, "labMap.csv"), header=0)
+
+"""
+Utilities to manipulating data.
+"""
+def add_aligned_time_steps_column(df, qfield, datefield, gte):
+    """
+    This function adds a new column, called `timestep`, that represent the number of time steps
+    after the cumulative value of `qfield` becomes equal or larger than `gte` value. `0` represents
+    a time point when the value meats the `gte` condition, `1` represents the next day, and so on.
+
+    df: Pandas DataFrame to manipulate data
+    qfield: A field that contains quantitative values
+    datefield: A field that contains date in a "YYYY-MM-DD" format.
+    gte: The value to compare
+    """
+
+    ######## Debug #######
+    # df = read_latest_daily_counts_df()
+    # qfield = "new_positive_cases"
+    # datefield = "date"
+    # gte = 10
+    ######################
+
+    timestep = "timestep"
+    df_copy = df.copy(deep=True)
+    df_copy[timestep] = -1
+
+    SITE_IDS = df[COLUMNS.SITE_ID].unique().tolist()
+
+    for siteid in SITE_IDS:
+        # Get only the rows for this siteid & sort them by date
+        df_by_site = df_copy[df_copy["siteid"] == siteid]
+        df_by_site = df_by_site.sort_values(by=datefield, ascending=True)
+
+        cumulative_value = 0
+        reference_date = ""
+        for idx in df_by_site.index:
+            cumulative_value += df_copy[qfield][idx]
+            if cumulative_value > gte:
+                reference_date = df_copy[datefield][idx]
+                break
+
+        if reference_date != "":
+            for idx in df_by_site.index:
+                ref_date_obj = datetime.datetime.strptime(reference_date, '%Y-%m-%d')
+                cur_date_obj = datetime.datetime.strptime(df_copy[datefield][idx], '%Y-%m-%d')
+
+                df_copy.loc[idx, 'timestep'] = (cur_date_obj - ref_date_obj).days
+
+    return df_copy
+
+"""
+Utilities to customize visualizations.
+"""
+def apply_theme(base, legend_orient="top-left"):
+    return base.configure_axis(
+        labelFontSize=14,
+        labelFontWeight=300,
+        titleFontSize=18,
+        titleFontWeight=300,
+        labelLimit=1000,
+    ).configure_title(fontSize=18, fontWeight=400, anchor="middle"
+    ).configure_legend(
+        titleFontSize=18, titleFontWeight=400,
+        labelFontSize=16, labelFontWeight=300,
+        padding=10,
+        cornerRadius=0,
+        orient=legend_orient
+    ).configure_view(
+        # stroke="black",
+        strokeWidth=1
+    )
