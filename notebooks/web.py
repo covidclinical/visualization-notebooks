af3211e1a20d241ec50ca47d9ae3f038ad52ac81
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-04-07 23:03:18 -0400

    Simplify for_website function

--- notebooks/web.py
+++ notebooks/web.py
@@ -23,24 +23,10 @@ def for_website(altair_plot, nb_name=None, plot_name=None):
     if should_save_for_web:
 
         vega_json = altair_plot.to_dict()
-        data = list(vega_json['data'].values())
-        assert(len(data) == 1)
-
-        dataset = list(vega_json['datasets'].values())[0]
 
         plot_id = f"{nb_name}_{plot_name}"
 
-        data_out_file = f"data-{plot_id}.json"
-        plot_out_file = f"plot-{plot_id}.json"
+        plot_out_file = f"plot_{plot_id}.json"
 
-        with open(join(save_dir, data_out_file), "w") as f:
-            json.dump(dataset, f)
-
-        del vega_json['data']
-        del vega_json['datasets']
-
-        vega_json['data'] = {
-            'url': data_out_file
-        }
         with open(join(save_dir, plot_out_file), "w") as f:
             json.dump(vega_json, f)
