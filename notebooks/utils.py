a8686c4b83417fd9fe27a739893cb0ee6b5614f7
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-04-11 18:43:18 -0400

    Remove unused codes in utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -224,83 +224,6 @@ def read_combined_by_site_labs_df():
     return read_combined_file_df(ft=SITE_FILE_TYPES.LABS, agg=DATA_AGGREGATE_TYPES.COMBINED_BY_SITE)
 
 """
-Helpers for preprocessing datasets before visualizing them.
-"""
-def preprocess_demographics_df_for_vis(df):
-    # Use more readable column names
-    df = df.rename(columns={
-        COLUMNS.AGE_0TO2: "0-2",
-        COLUMNS.AGE_3TO5: "3-5",
-        COLUMNS.AGE_6TO11: "6-11",
-        COLUMNS.AGE_12TO17: "12-17",
-        COLUMNS.AGE_18TO25: "18-25",
-        COLUMNS.AGE_26TO49: "26-49",
-        COLUMNS.AGE_50TO69: "50-69",
-        COLUMNS.AGE_70TO79: "70-79",
-        COLUMNS.AGE_80PLUS: "80+"
-    })
-    # Use the consistent capitalization
-    df[COLUMNS.SEX] = df[COLUMNS.SEX].apply(lambda x: x.capitalize())
-
-    # Remove aggregated rows and columns
-    not_all_filter = df[COLUMNS.SEX] != "All"
-    df = df[not_all_filter]
-    df = df.drop(columns=[COLUMNS.TOTAL_PATIENTS])
-
-    # Wide to long
-    df = pd.melt(df, id_vars=[COLUMNS.SITE_ID, COLUMNS.SEX])
-    df = df.rename(columns={"variable": COLUMNS.AGE_GROUP, "value": COLUMNS.NUM_PATIENTS})
-    return df
-
-def preprocess_daily_counts_df_for_vis(df):
-    # Change variables to more readable names
-    df = df.rename(columns={
-        # TODO: Do we need to do this here?
-        COLUMNS.NEW_POSITIVE_CASES: "New positive cases",
-        COLUMNS.PATIENTS_IN_ICU: "Patients in ICU",
-        COLUMNS.NEW_DEATHS: "New deaths"
-    })
-
-    # Add a column that use relative time point
-    df = add_aligned_time_steps_column(df, "New positive cases", "date", 1)
-
-    # Convert uncertain data to zero
-    df.loc[df["New positive cases"] < 0, "New positive cases"] = 0
-    df.loc[df["Patients in ICU"] < 0, "Patients in ICU"] = 0
-    df.loc[df["New deaths"] < 0, "New deaths"] = 0
-
-    # wide to long
-    df = pd.melt(df, id_vars=[COLUMNS.SITE_ID, COLUMNS.DATE, "timestep"])
-    df = df.rename(columns={"variable": "category", "value": COLUMNS.NUM_PATIENTS})
-    return df
-
-def preprocess_diagnoses_df_for_vis(df):
-    # Our lookup table does not contain dots
-    df[COLUMNS.ICD_CODE] = df[COLUMNS.ICD_CODE].apply(lambda x: x.replace(".", ""))
-
-    # Merge with a lookup table
-    icd_df = read_icd_df()
-    df = df.merge(icd_df, how="left", left_on=COLUMNS.ICD_CODE, right_on="ICDcode")
-
-    # Handle the missing data
-    df.loc[pd.isna(df["ICDdescription"]), "ICDdescription"] = df.loc[pd.isna(df["ICDdescription"]), COLUMNS.ICD_CODE]
-    df.loc[pd.isna(df["Category"]), "Category"] = df.loc[pd.isna(df["Category"]), COLUMNS.ICD_CODE]
-
-    # Consistent capitalization
-    df["ICDdescription"] = df["ICDdescription"].apply(lambda x: x.capitalize())
-    df["Category"] = df["Category"].apply(lambda x: x.capitalize())
-    return df
-
-def preprocess_labs_df_for_vis(df):
-    # Add descriptive columns
-    _loinc_df = read_loinc_df().set_index('loinc').rename(columns={'labTest': 'name'})
-    df["loinc_name"] = df[COLUMNS.LOINC].apply(lambda code: _loinc_df.at[code, "name"].capitalize())
-
-    # Convert uncertain data to zero
-    df.loc[df[COLUMNS.NUM_PATIENTS] < 0, COLUMNS.NUM_PATIENTS] = 0
-    return df
-
-"""
 Helpers for reading additional data files.
 """
 def read_icd_df():
@@ -413,81 +336,37 @@ def get_combined_columns(filetype):
         ]
 
 """
-Utilities to manipulating data.
-"""
-def add_aligned_time_steps_column(df, qfield, datefield, gte):
-    """
-    This function adds a new column, called `timestep`, that represent the number of time steps
-    after the cumulative value of `qfield` becomes equal or larger than `gte` value. `0` represents
-    a time point when the value meats the `gte` condition, `1` represents the next day, and so on.
-
-    df: Pandas DataFrame to manipulate data
-    qfield: A field that contains quantitative values
-    datefield: A field that contains date in a "YYYY-MM-DD" format.
-    gte: The value to compare
-    """
-
-    ######## Debug #######
-    # df = read_latest_daily_counts_df()
-    # qfield = "new_positive_cases"
-    # datefield = "date"
-    # gte = 10
-    ######################
-
-    timestep = "timestep"
-    df_copy = df.copy(deep=True)
-    df_copy[timestep] = -1
-
-    SITE_IDS = df[COLUMNS.SITE_ID].unique().tolist()
-
-    for siteid in SITE_IDS:
-        # Get only the rows for this siteid & sort them by date
-        df_by_site = df_copy[df_copy["siteid"] == siteid]
-        df_by_site = df_by_site.sort_values(by=datefield, ascending=True)
-
-        cumulative_value = 0
-        reference_date = ""
-        for idx in df_by_site.index:
-            cumulative_value += df_copy[qfield][idx]
-            if cumulative_value > gte:
-                reference_date = df_copy[datefield][idx]
-                break
-
-        if reference_date != "":
-            for idx in df_by_site.index:
-                ref_date_obj = reference_date
-                cur_date_obj = df_copy[datefield][idx]
-
-                df_copy.loc[idx, 'timestep'] = (cur_date_obj - ref_date_obj).days
-
-    return df_copy
-
-"""
 Utilities to customize visualizations.
 """
 def apply_theme(
     base,
+    title_anchor="start",
+    title_font_size=18,
+    axis_title_font_size=16,
+    axis_label_font_size=14,
     legend_orient="top-left",
     legend_stroke_color="lightgray",
     legend_padding=10,
-    label_font_size=14,
-    axis_title_font_size=16,
-    title_anchor="start",
-    title_font_size=18,
-    axis_label_font_size=14
+    label_font_size=14
 ):
-    return base.configure_axis(
+    return base.configure_view(
+        # ...
+    ).configure_header(
+        titleFontSize=16,
+        titleFontWeight=300,
+        labelFontSize=13
+    ).configure_title(
+        fontSize=title_font_size,
+        fontWeight=400,
+        anchor=title_anchor,
+        align="left"
+    ).configure_axis(
         labelFontSize=axis_label_font_size,
         labelFontWeight=300,
         titleFontSize=axis_title_font_size,
         titleFontWeight=300,
         labelLimit=1000,
         labelAngle=0
-    ).configure_title(
-        fontSize=title_font_size,
-        fontWeight=400,
-        anchor=title_anchor,
-        align="left"
     ).configure_legend(
         titleFontSize=16, titleFontWeight=400,
         labelFontSize=label_font_size, labelFontWeight=300,
@@ -496,86 +375,6 @@ def apply_theme(
         orient=legend_orient,
         fillColor="white",
         strokeColor=legend_stroke_color
-    ).configure_view(
     ).configure_concat(
         spacing=0
-    )
-
-def apply_trellis_theme(base):
-    return base.configure_axis(
-        labelFontSize=14,
-        labelFontWeight=300,
-        titleFontSize=18,
-        titleFontWeight=400,
-        labelLimit=1000,
-        gridColor="#f0f0f0",
-        # gridOpacity=0,
-        domainOpacity=0,
-        # tickOpacity=0
-        tickColor="lightgray"
-        # grid=False
-    ).configure_title(
-        fontSize=22, fontWeight=400, anchor="start",
-        dx=100
-    ).configure_legend(
-        titleFontSize=18, titleFontWeight=400,
-        labelFontSize=16, labelFontWeight=300,
-        padding=10,
-        cornerRadius=0,
-        labelLimit=1000,
-    ).configure_view(
-        stroke="lightgray",
-        strokeWidth=2
-    ).configure_header(
-        labelFontSize=18,
-    ).configure_concat(
-        spacing=-100
-    ).configure_facet(
-        spacing=6
-    )
-
-def apply_grouped_bar_theme(base, legend_orient="top-left", strokeColor=None):
-    return base.configure_axis(
-        labelFontSize=14,
-        labelFontWeight=300,
-        titleFontSize=18,
-        titleFontWeight=300,
-        labelLimit=1000,
-    ).configure_title(fontSize=18, fontWeight=400, align="left", anchor="start"
-    ).configure_legend(
-        titleFontSize=18, titleFontWeight=400,
-        labelFontSize=16, labelFontWeight=300,
-        fillColor="white",
-        strokeColor=strokeColor,
-        padding=10,
-        cornerRadius=0,
-        orient=legend_orient
-    ).configure_view(
-    ).configure_header(
-        titleFontSize=16,
-        titleFontWeight=300,
-        labelFontSize=13
-    )
-
-def apply_pyramid_theme(base, legend_orient="top-left", strokeColor=None):
-    return base.configure_axis(
-        labelFontSize=14,
-        labelFontWeight=300,
-        titleFontSize=18,
-        titleFontWeight=300,
-        labelLimit=1000,
-    ).configure_title(fontSize=18, fontWeight=400, anchor="middle"
-    ).configure_legend(
-        titleFontSize=18, titleFontWeight=400,
-        labelFontSize=16, labelFontWeight=300,
-        fillColor="white",
-        strokeColor=strokeColor,
-        padding=10,
-        cornerRadius=0,
-        orient=legend_orient
-    ).configure_view(
-    ).configure_header(
-        titleFontSize=16,
-        titleFontWeight=300,
-        labelFontSize=13
     )
